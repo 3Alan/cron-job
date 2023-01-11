9e5fcdef6deec6d64fed15cdbce56d541101761c
3Alan <1540703192@qq.com>
2023-01-11 18:57:17 +0800

    debug error

--- pages/api/juejin.ts
+++ pages/api/juejin.ts
@@ -26,68 +26,77 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   }
   res.status(200).json({ success: true, message: 'executing...' });
 
-  const LOCAL_CHROME_EXECUTABLE =
-    process.platform === 'win32'
-      ? 'C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe'
-      : process.platform === 'linux'
-      ? '/usr/bin/google-chrome'
-      : '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome';
+  try {
+    const LOCAL_CHROME_EXECUTABLE =
+      process.platform === 'win32'
+        ? 'C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe'
+        : process.platform === 'linux'
+        ? '/usr/bin/google-chrome'
+        : '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome';
 
-  const executablePath = (await chromium.executablePath) || LOCAL_CHROME_EXECUTABLE;
+    const executablePath = (await chromium.executablePath) || LOCAL_CHROME_EXECUTABLE;
 
-  const browser = await puppeteer.launch({
-    executablePath,
-    headless: false,
-    slowMo: 250, // slow down by 250ms
-    defaultViewport: { width: 1440, height: 1000 },
-    args: chromium.args
-  });
-  const page = await browser.newPage();
-  await page.setDefaultNavigationTimeout(0);
-  // page.on('console', msg => console.log('PAGE LOG:', JSON.stringify(msg)));
+    const browser = await puppeteer.launch({
+      executablePath,
+      headless: false,
+      slowMo: 250, // slow down by 250ms
+      defaultViewport: { width: 1440, height: 1000 },
+      args: chromium.args
+    });
+    const page = await browser.newPage();
+    await page.setDefaultNavigationTimeout(0);
+    // page.on('console', msg => console.log('PAGE LOG:', JSON.stringify(msg)));
 
-  const cookieArgs = JSON.parse(process.env.JUEJIN_COOKIE_JSON || '[]').map((item: any) => ({
-    name: item.name,
-    value: item.value,
-    domain: item.domain,
-    path: item.path,
-    expires: item.expirationDate,
-    httpOnly: item.httpOnly,
-    secure: item.secure,
-    sameSite: item.sameSite
-  }));
-  await page.setCookie(...cookieArgs);
+    const cookieArgs = JSON.parse(process.env.JUEJIN_COOKIE_JSON || '[]').map((item: any) => ({
+      name: item.name,
+      value: item.value,
+      domain: item.domain,
+      path: item.path,
+      expires: item.expirationDate,
+      httpOnly: item.httpOnly,
+      secure: item.secure,
+      sameSite: item.sameSite
+    }));
+    await page.setCookie(...cookieArgs);
 
-  await page.goto('https://juejin.cn/user/center/signin?from=main_page', {
-    waitUntil: 'domcontentloaded'
-  });
-  // 页面有动画
-  await sleep(1000);
+    await page.goto('https://juejin.cn/user/center/signin?from=main_page', {
+      waitUntil: 'domcontentloaded'
+    });
+    // 页面有动画
+    await sleep(1000);
 
-  const isComplete = (await (await page.$$('button[class="signin btn"]')).length) === 0;
-  if (!isComplete) {
-    // 签到操作
-    await page.click('button[class="signin btn"]');
+    const isComplete = (await (await page.$$('button[class="signin btn"]')).length) === 0;
+    if (!isComplete) {
+      // 签到操作
+      await page.click('button[class="signin btn"]');
+    }
+    const checkInImgBuffer = (await page.screenshot()) as Buffer;
+
+    // 沾喜气
+    await page.goto('https://juejin.cn/user/center/lottery?from=lucky_lottery_menu_bar', {
+      waitUntil: 'domcontentloaded'
+    });
+    await sleep(1000, true);
+    await page.click('svg[class="stick-btn"]');
+    const lotteryImgBuffer = (await page.screenshot()) as Buffer;
+
+    await sendEmail({
+      from: process.env.EMAIL_FROM,
+      to: process.env.EMAIL_TO,
+      subject: '定时任务通知 ✅',
+      html: `<img src="data:image/png;base64,${checkInImgBuffer.toString(
+        'base64'
+      )}" /><img src="data:image/png;base64,${lotteryImgBuffer.toString('base64')}" />`
+    });
+
+    await page.close();
+    await browser.close();
+  } catch (err: any) {
+    await sendEmail({
+      from: process.env.EMAIL_FROM,
+      to: process.env.EMAIL_TO,
+      subject: '定时任务通知 ✅',
+      html: err.message
+    });
   }
-  const checkInImgBuffer = (await page.screenshot()) as Buffer;
-
-  // 沾喜气
-  await page.goto('https://juejin.cn/user/center/lottery?from=lucky_lottery_menu_bar', {
-    waitUntil: 'domcontentloaded'
-  });
-  await sleep(1000, true);
-  await page.click('svg[class="stick-btn"]');
-  const lotteryImgBuffer = (await page.screenshot()) as Buffer;
-
-  await sendEmail({
-    from: process.env.EMAIL_FROM,
-    to: process.env.EMAIL_TO,
-    subject: '定时任务通知 ✅',
-    html: `<img src="data:image/png;base64,${checkInImgBuffer.toString(
-      'base64'
-    )}" /><img src="data:image/png;base64,${lotteryImgBuffer.toString('base64')}" />`
-  });
-
-  await page.close();
-  await browser.close();
 }
