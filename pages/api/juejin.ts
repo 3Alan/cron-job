78a29f224f98e45931523072af28356cc373d31d
3Alan <1540703192@qq.com>
2023-01-11 16:37:45 +0800

    test vercel

---
+++ pages/api/juejin.ts
@@ -0,0 +1,60 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import puppeteer from 'puppeteer';
+
+function sleep(ms: number) {
+  return new Promise(resolve => {
+    setTimeout(resolve, ms);
+  });
+}
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const browser = await puppeteer.launch({
+    headless: false,
+    devtools: true,
+    slowMo: 250, // slow down by 250ms
+    defaultViewport: { width: 1440, height: 1000 },
+    args: ['--no-sandbox', '--disable-setuid-sandbox']
+  });
+  const page = await browser.newPage();
+  await page.setDefaultNavigationTimeout(0);
+  page.on('console', msg => console.log('PAGE LOG:', JSON.stringify(msg)));
+
+  const cookieArgs = JSON.parse(process.env.JUEJIN_COOKIE_JSON || '[]').map((item: any) => ({
+    name: item.name,
+    value: item.value,
+    domain: item.domain,
+    path: item.path,
+    expires: item.expirationDate,
+    httpOnly: item.httpOnly,
+    secure: item.secure,
+    sameSite: item.sameSite
+  }));
+  await page.setCookie(...cookieArgs);
+
+  await page.goto('https://juejin.cn/user/center/signin?from=main_page', {
+    waitUntil: 'domcontentloaded'
+  });
+
+  await page.screenshot();
+  const isComplete = (await (await page.$$('button[class="signin btn"]')).length) === 0;
+
+
+  // 沾喜气
+  await page.goto('https://juejin.cn/user/center/lottery?from=lucky_lottery_menu_bar', {
+    waitUntil: 'domcontentloaded'
+  });
+  await sleep(1000);
+  await page.click('svg[class="stick-btn"]');
+
+  if (isComplete) {
+    page.evaluate(async () => {
+      //打印日志
+      console.log('isComplete');
+    });
+  } else {
+    // 签到操作
+    await page.click('button[class="signin btn"]');
+  }
+  await browser.close();
+  res.status(200).json({ success: true, isComplete });
+}
