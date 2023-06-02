f7bfff1a0ab1b589e7e8bc1398a409b1d0fd50d5
3Alan <1540703192@qq.com>
2023-06-02 09:29:28 +0800

    test

--- src/juejin.js
+++ src/juejin.js
@@ -67,7 +67,7 @@ async function getLucky(page) {
 
 export default async function juejin() {
   const browser = await puppeteer.launch({
-    headless: !isDev(),
+    // headless: !isDev(),
     slowMo: isDev() ? 250 : 0, // slow down by 250ms
     defaultViewport: { width: 1440, height: 1200 }
   });
